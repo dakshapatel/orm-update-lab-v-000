f39e5ae3bb1215b8274075ad02ae7b8788eb7f31
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 18:03:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,11 +46,10 @@ class Student
   end
 
   def self.new_from_db(row)
-
-    new_name.id = row[0]
-    new_name.name = row[1]
-    new_name.grade = row[2]
-    new_name = self.new(name, grade)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    new_name = self.new(name, grade, id)
     new_name
   end
 
